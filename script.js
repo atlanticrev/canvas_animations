b457ee951f065ed7b82be376d2428eb3a55e442c
Atlanticrev <atlanticrev@gmail.com>
2019-06-04 18:37:04 +0300

    Подправил код

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-/*
-* Definitions
-*/
+
+/******************
+ ***  Definitions
+ *******************/
+
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -8,65 +10,73 @@ canvas.height = window.innerHeight;
 
 const pi = Math.PI;
 
-function random (start, stop) {
-	// Установить длину интервала а потом передвинуть его
-	return Math.random() * (stop - start) + start;
+function random(start, stop) {
+  // Установить длину интервала а потом передвинуть его
+  return Math.random() * (stop - start) + start;
 }
 
-function randomColor () {
-	return [random(0, 255), random(0, 255), random(0, 255)];
+function randomColor() {
+  return [
+    random(0, 255),
+    random(0, 255),
+    random(0, 255)
+  ];
 }
 
-function circleFactory (quantity) {
-	let result = [];
+function circleFactory(quantity) {
+  let result = [];
 
-	for (let i = 0; i < quantity; i++) {
-		result.push(new Circle(
-				random(0, innerWidth - random(2, 30)),
-				random(0, innerHeight - random(2, 30)), random(2, 30),
-				random(0, 12) - 6, random(0, 12) - 6, randomColor()
-			)
-		);
-	}
+  for (let i = 0; i < quantity; i++) {
+    result.push(new Circle(
+      random(0, innerWidth - random(2, 30)),
+      random(0, innerHeight - random(2, 30)),
+      random(2, 30),
+      random(0, 12) - 6, random(0, 12) - 6,
+      randomColor()
+      )
+    );
+  }
 
-	return result;
+  return result;
 }
 
-function Circle (x, y, radius, dx, dy, color) {
-	this.x = x;
-	this.y = y;
-	this.radius = radius;
-	this.dx = dx;
-	this.dy = dy;
-	this.color = 'rgb(' + color.toString() + ')';
+function Circle(x, y, radius, dx, dy, color) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.dx = dx;
+  this.dy = dy;
+  this.color = 'rgb(' + color.toString() + ')';
 }
 
 Circle.prototype.draw = function () {
-	ctx.beginPath();
-	ctx.arc(this.x, this.y, this.radius, 0, 2*pi, false);
-	ctx.strokeStyle = this.color;
-	ctx.lineWidth = 2;
-	ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.radius, 0, 2 * pi, false);
+  ctx.strokeStyle = this.color;
+  ctx.lineWidth = 2;
+  ctx.stroke();
 };
 
 Circle.prototype.update = function () {
-	// x + radius - левая граница окружности, x - radius правая граница окружности
-	if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
-		// Меняю направление скорости, если окружность доходит до края справа или слева
-		this.dx = -this.dx;
-	}
+// x + radius - левая граница окружности, x - radius правая граница окружности
 
-	if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
-		this.dy = -this.dy;
-	}
+  if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
+    // Меняю направление скорости, если окружность доходит до края справа или слева
+    this.dx = -this.dx;
+  }
 
-	this.x += this.dx;
-	this.y += this.dy;
+  if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+    // Меняю направление скорости, если окружность доходит до края сверху или снизу
+    this.dy = -this.dy;
+  }
+
+  this.x += this.dx;
+  this.y += this.dy;
 };
 
-/*
-* Drawing
-*/
+/******************
+ ***	Drawing
+ *******************/
 
 /* ctx.beginPath();
 ctx.moveTo(100, window.innerHeight / 2);
@@ -90,14 +100,14 @@ ctx.stroke(); */
 
 let circles = circleFactory(250);
 
-function animate () {
-	requestAnimationFrame(animate);
-	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-	circles.forEach(function (circle) {
-		circle.draw();
-		circle.update();
-	});
+  circles.forEach(function (circle) {
+    circle.draw();
+    circle.update();
+  });
 }
 
 animate();
