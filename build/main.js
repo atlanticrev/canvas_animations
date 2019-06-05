d5b4400fff4e78674fac79b6fc6e23dce9beaa08
Atlanticrev <atlanticrev@gmail.com>
2019-06-05 11:56:46 +0300

    Перенес рисунки не относящиеся к кружкам в отдельный файл

--- build/main.js
+++ build/main.js
@@ -227,26 +227,6 @@ canvas.height = window.innerHeight;
  ***	Drawing
  *******************/
 
-/* context.beginPath();
-context.moveTo(100, window.innerHeight / 2);
-context.lineTo(600, window.innerHeight / 2 - 150);
-context.lineTo(600, window.innerHeight / 2 + 150);
-context.lineTo(100, window.innerHeight / 2);
-context.lineTo(600, window.innerHeight / 2);
-context.strokeStyle = "blue";
-context.stroke(); */
-
-/* for (let i = 0; i < 100; i++) {
-	let radius = random(10, 50);
-	let x = random(radius, window.innerWidth - 50);
-	let y = random(radius, window.innerHeight - 50);
-
-	context.beginPath();
-	context.arc(x, y, radius, 0, 2*Math.PI, false);
-	context.strokeStyle = 'rgb(' + randomColor().toString() + ')';
-	context.stroke();
-} */
-
 var circles = Object(_circleFactory__WEBPACK_IMPORTED_MODULE_0__["circleFactory"])(250);
 
 function animate() {
--- script.js
+++ script.js
@@ -13,26 +13,6 @@ canvas.height = window.innerHeight;
  ***	Drawing
  *******************/
 
-/* context.beginPath();
-context.moveTo(100, window.innerHeight / 2);
-context.lineTo(600, window.innerHeight / 2 - 150);
-context.lineTo(600, window.innerHeight / 2 + 150);
-context.lineTo(100, window.innerHeight / 2);
-context.lineTo(600, window.innerHeight / 2);
-context.strokeStyle = "blue";
-context.stroke(); */
-
-/* for (let i = 0; i < 100; i++) {
-	let radius = random(10, 50);
-	let x = random(radius, window.innerWidth - 50);
-	let y = random(radius, window.innerHeight - 50);
-
-	context.beginPath();
-	context.arc(x, y, radius, 0, 2*Math.PI, false);
-	context.strokeStyle = 'rgb(' + randomColor().toString() + ')';
-	context.stroke();
-} */
-
 let circles = circleFactory(250);
 
 function animate() {
